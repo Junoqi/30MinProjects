18fae6f8e6f41d246ca36b55baf6f7a65543f283
Henry Pope IV <henrypope4th@gmail.com>
2021-02-17 16:44:23 +0000

    start function renaming

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -2,92 +2,98 @@ import random
 import replit
 import os
 
-
 #Global Variables
 options = ["Rock", "Paper", "Scissors"]
 
+
 #Clear function
 def cls():
-    os.system('cls' if os.name=='nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-#In a function so i can loop
+
+#Start function
 def start():
-  print("WELCOME TO ROCK PAPER SCISSORS! ")
-  play("play")
+    print("WELCOME TO ROCK PAPER SCISSORS! ")
+    play("play")
+
 
 #Player Win
 def playerWin():
-  print("You won!! Play again? (Y/N): ")
-  playAgain = input()
+    print("You won!! Play again? (Y/N): ")
+    playAgain = input()
 
-  if(playAgain.lower() == "y"):
-    try:
-      replit.clear()
-    except:
-      cls()
-    start()
+    if (playAgain.lower() == "y"):
+        try:
+            replit.clear()
+        except:
+            cls()
+        start()
 
-  elif(playAgain.lower() == "n"):
-    return
+    elif (playAgain.lower() == "n"):
+        return
+
+
 #CPU Win
 def cpuWin():
-  print("Uh-oh! You lost!! Play again? (Y/N): ")
-  playAgain = input()
+    print("Uh-oh! You lost!! Play again? (Y/N): ")
+    playAgain = input()
 
-  if(playAgain.lower() == "y"):
-    try:
-      replit.clear()
-    except:
-      cls()
-    start()
-  elif(playAgain.lower() == "n"):
-    return
+    if (playAgain.lower() == "y"):
+        try:
+            replit.clear()
+        except:
+            cls()
+        start()
+    elif (playAgain.lower() == "n"):
+        return
+
 
 #Game Loop
 def play(type):
-  if(type == "again"):
-    print("Same Input! Again!")
-  else:
-    pass
+    if (type == "again"):
+        print("Same Input! Again!")
+    else:
+        pass
 
-  cpuChoice = random.choice(options)
-  #PlayerInput
-  print("Rock, Paper or Scissors?: ")
-  playerChoice = input()
+    cpuChoice = random.choice(options)
+    #PlayerInput
+    print("Rock, Paper or Scissors?: ")
+    playerChoice = input()
 
-  #Speech
-  print("-------")
-  print("Rock...")
-  print("Paper...")
-  print("Scissors...")
-  print("SHOOT!")
-  print("-------")
+    #Speech
+    print("-------")
+    print("Rock...")
+    print("Paper...")
+    print("Scissors...")
+    print("SHOOT!")
+    print("-------")
 
-  print(f"{playerChoice.upper()} (You) VS {cpuChoice.upper()} (CPU)")
+    print(f"{playerChoice.upper()} (You) VS {cpuChoice.upper()} (CPU)")
 
-  print("-------")
+    print("-------")
 
-  #Logic
-  if(cpuChoice.lower() == playerChoice.lower()):
-    try:
-      replit.clear()
-    except:
-      cls()
-    play("again")
-  elif(cpuChoice.lower() == "rock" and playerChoice.lower() == "paper"):
-    playerWin()
-  elif(cpuChoice.lower() == "paper" and playerChoice.lower() == "scissors"):
-    playerWin()
-  elif(cpuChoice.lower() == "scissors" and playerChoice.lower() == "rock"):
-    playerWin()
-  elif(cpuChoice.lower() == "paper" and playerChoice.lower() == "rock"):
-    cpuWin()
-  elif(cpuChoice.lower() == "rock" and playerChoice.lower() == "scissors"):
-    playerWin()
-  elif(cpuChoice.lower() == "scissors" and playerChoice.lower() == "paper"):
-    playerWin()
-  else:
-    print("INVALID INPUT")
-    play("play")
+    #Logic
+    if (cpuChoice.lower() == playerChoice.lower()):
+        try:
+            replit.clear()
+        except:
+            cls()
+        play("again")
+    elif (cpuChoice.lower() == "rock" and playerChoice.lower() == "paper"):
+        playerWin()
+    elif (cpuChoice.lower() == "paper" and playerChoice.lower() == "scissors"):
+        playerWin()
+    elif (cpuChoice.lower() == "scissors" and playerChoice.lower() == "rock"):
+        playerWin()
+    elif (cpuChoice.lower() == "paper" and playerChoice.lower() == "rock"):
+        cpuWin()
+    elif (cpuChoice.lower() == "rock" and playerChoice.lower() == "scissors"):
+        playerWin()
+    elif (cpuChoice.lower() == "scissors" and playerChoice.lower() == "paper"):
+        playerWin()
+    else:
+        print("INVALID INPUT")
+        play("play")
+
 
 start()
